90761e781f08f147a1619ee0e4ca3f939e8d7f2a
Smiti <mishra.smiti06@gmail.com>
2022-02-17 19:05:56 -0500

    Add favourite movie

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { useEffect, useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeadings';
+import SearchBox from './components/SearchBox';
+import AddToFavourites from './components/AddToFavourites';
+
+
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+  const [favourites, setFavourites] = useState([]);
+
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=72e471e9`;
+    const response = await fetch(url);
+    const responseJson = await response.json();
+    if (responseJson.Search) {
+			setMovies(responseJson.Search);
+		}
+  } 
+
+  const addFavouriteMovie = (movie) => {
+		const newFavouriteList = [...favourites, movie];
+		setFavourites(newFavouriteList);
+	};
+
+
+  useEffect(() => {
+    getMovieRequest(searchValue);
+  }, [searchValue]);
+  
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+		<div className='container-fluid movie-app'>
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+        <MovieListHeading heading='Movies'/>
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
+      </div>
+			<div className='row'>
+				<MovieList movies={movies} 
+        favouriteComponent={AddToFavourites} 
+        handleFavouritesClick={addFavouriteMovie}
+        />
+			</div>
+		</div>
+	);
+};
 
 export default App;
